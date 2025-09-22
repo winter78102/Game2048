b001e81cbfbc5820577bffc99f912f4cd0d41f33
Bahar Dehghanpour <bahar.d1378@gmail.com>
2025-09-22 17:14:59 +0330

    remove build folder

--- build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Game_FILE C:/Users/Bahar/Documents/Game/build/Desktop_Qt_6_9_2_MinGW_64_bit-Debug/Game.exe)
-set(__QT_DEPLOY_TARGET_Game_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Game_RUNTIME_DLLS C:/Qt/6.9.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.2/mingw_64/bin/Qt6Core.dll)
